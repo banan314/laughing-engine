01ebbc8713de9e1257947d75d2160ba9c4e345dc
banan314 <klopusz492@outlook.com>
2017-06-30 13:42:50 +0200

    calculate territory correctly

--- src/main/java/pl/edu/prz/klopusz/utilities/decorators/TerritoryCalculatorImpl.java
+++ src/main/java/pl/edu/prz/klopusz/utilities/decorators/TerritoryCalculatorImpl.java
@@ -4,7 +4,6 @@ import pl.edu.prz.klopusz.application.model.game.Square;
 import pl.edu.prz.klopusz.application.model.game.Board;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -16,6 +15,8 @@ public class TerritoryCalculatorImpl implements TerritoryCalculator {
     public static final int MIN_INDEX = 1;
     public static final int MAX_INDEX = 9;
 
+    public static boolean DEBUG = false;
+
     private final Board board;
     private CellState[][] cellBoard = new CellState[10][10]; //board is 9x9, but we index from 1
     private CellState[][] rememberedBoard = new CellState[10][10];
@@ -26,33 +27,195 @@ public class TerritoryCalculatorImpl implements TerritoryCalculator {
         this.board = board;
     }
 
-    //FIXME: the cell state board is changing every time we process single cell. Should work on remembered board from
-    // previous step
     /*TODO: test it!!!!!!*/
+    /*FIXME: cell 1,1 is empty sometimes (initially)*/
     @Override
     public void calculate() {
         initializeCellBoard();
         copyStatesFromGameBoard();
-        copyToRemembered();
 
-        while(shouldContinueCalculation()) {
+        if (DEBUG) {
+            System.out.println("---------------------------------");
+        }
+        do {
+            copyToRemembered();
             traverseBoard((i, j, state) -> {
                 processCellState(i, j, state);
             });
-            copyToRemembered();
+            if (DEBUG) {
+                debugBoardState();
+            }
+        } while(shouldContinueCalculation());
+
+        if (DEBUG) {
+            System.out.println("---------------------------------");
         }
 
-        ExtendedInt scoreBlack = new ExtendedInt(0),
-            scoreWhite = new ExtendedInt(0);
+        ExtendedInt scoreBlack = new ExtendedInt(0), scoreWhite = new ExtendedInt(0);
         traverseBoard((i, j, state) -> {
-            if(state == CellState.SEEDED_WHITE)
+            if (state == CellState.SEEDED_WHITE) {
                 scoreWhite.increment();
-            else if(state == CellState.SEEDED_BLACK)
+            } else if (state == CellState.SEEDED_BLACK) {
                 scoreBlack.increment();
+            }
         });
 
         whiteScore = scoreWhite.get();
         blackScore = scoreBlack.get();
+    }
+
+    private void initializeCellBoard() {
+        traverseBoard((i, j, state) -> {
+            cellBoard[i][j] = CellState.EMPTY;
+        });
+    }
+
+    private void copyStatesFromGameBoard() {
+        traverseBoard((i, j, state) -> {
+            Square square = board.get(i, j);
+            if (!square.isEmpty()) {
+                if (square.getPiece().isBlack()) {
+                    cellBoard[i][j] = CellState.OCCUPIED_BLACK;
+                } else {
+                    assert square.getPiece().isWhite();
+                    cellBoard[i][j] = CellState.OCCUPIED_WHITE;
+                }
+            }
+        });
+    }
+
+    private void processCellState(final int file, final int row, final CellState state) {
+        List<CellState> adjacent;
+        switch (state) {
+            case EMPTY:
+                adjacent = getNeighbors(file, row);
+                checkEmptyCellNeighbors(file, row, adjacent);
+                break;
+            case OCCUPIED_WHITE:
+            case OCCUPIED_BLACK:
+                //TODO: needed?
+                break;
+            case SEEDED_WHITE:
+            case SEEDED_BLACK:
+                adjacent = getNeighbors(file, row);
+                checkSeededCellNeighbors(file, row, state, adjacent);
+                break;
+            case CONTAMINATED:
+                cellBoard[file][row] = CellState.SHARED;
+                break;
+        }
+    }
+
+    private void checkSeededCellNeighbors(int file, int row, CellState state, List<CellState> adjacent) {
+        for(CellState neighbor : adjacent) {
+            if (neighbor.isDame()) {
+                cellBoard[file][row] = CellState.CONTAMINATED;
+            }
+            if (neighbor.isSeeded() && neighbor != state) {
+                cellBoard[file][row] = CellState.CONTAMINATED;
+            }
+        }
+    }
+
+    private void checkEmptyCellNeighbors(int file, int row, List<CellState> adjacent) {
+        boolean white = false, black = false, shared = false;
+        for(CellState square : adjacent) {
+            if (square.isDame()) {
+                shared = true;
+            }
+            if (square.isEmpty()) {
+                continue;
+            }
+            if (square.isWhite()) {
+                white = true;
+            }
+            if (square.isBlack()) {
+                black = true;
+            }
+        }
+        if (shared || (white && black)) {
+            cellBoard[file][row] = CellState.SHARED;
+        } else {
+            if (white) {
+                cellBoard[file][row] = CellState.SEEDED_WHITE;
+            } else if (black) {
+                cellBoard[file][row] = CellState.SEEDED_BLACK;
+            }
+            //else nothing can be said
+        }
+    }
+
+    protected List<CellState> getNeighbors(int file, int row) {
+//        return get4Neighbors(file, row);
+        return get8Neighbors(file, row);
+    }
+
+    private List<CellState> get8Neighbors(int file, int row) {
+        List<CellState> adjacent = get4Neighbors(file, row);
+
+        if (file > MIN_INDEX && row > MIN_INDEX) {
+            adjacent.add(cellBoard[file-1][row-1]);
+        }
+        if (file > MIN_INDEX && row < MAX_INDEX) {
+            adjacent.add(cellBoard[file-1][row+1]);
+        }
+        if (file < MAX_INDEX && row > MIN_INDEX) {
+            adjacent.add(cellBoard[file+1][row-1]);
+        }
+        if (file < MAX_INDEX && row < MAX_INDEX) {
+            adjacent.add(cellBoard[file+1][row+1]);
+        }
+
+        return adjacent;
+    }
+
+    private List<CellState> get4Neighbors(int file, int row) {
+        List<CellState> adjacent = new ArrayList<>();
+
+        if (file > MIN_INDEX) {
+            adjacent.add(rememberedBoard[file-1][row]);
+        }
+        if (file < MAX_INDEX) {
+            adjacent.add(rememberedBoard[file+1][row]);
+        }
+        if (row > MIN_INDEX) {
+            adjacent.add(rememberedBoard[file][row-1]);
+        }
+        if (row < MAX_INDEX) {
+            adjacent.add(rememberedBoard[file][row+1]);
+        }
+        return adjacent;
+    }
+
+    private boolean shouldContinueCalculation() {
+        Mutable<Boolean> should = new Mutable<>(false);
+        traverseBoard((i, j, state) -> {
+            /*if (state == CellState.EMPTY || state == CellState.CONTAMINATED) {
+                should.set(true);
+            }*/
+            if (state != cellBoard[i][j]) {
+                should.set(true);
+            }
+        });
+        return should.get();
+    }
+
+    private void copyToRemembered() {
+        for(int i = 1; i < 10; i++) {
+            for(int j = 1; j < 10; j++) {
+                rememberedBoard[i][j] = cellBoard[i][j];
+            }
+        }
+    }
+
+    private void debugBoardState() {
+        for(int j = 9; j > 0; j--) {
+            for(int k = 1; k < 10; k++) {
+                System.out.printf("%2s", rememberedBoard[k][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 
     @Override
@@ -65,107 +228,6 @@ public class TerritoryCalculatorImpl implements TerritoryCalculator {
         return blackScore;
     }
 
-    private void processCellState(int file, int row, CellState state) {
-        List<CellState> adjacent;
-        switch (state) {
-            case EMPTY:
-                //TODO: should take 8-neighbors
-                adjacent = getNeighbors(file, row);
-                boolean white = false, black = false, shared = false;
-                for(CellState square : adjacent) {
-                    if(square.isDame())
-                    if (square.isEmpty()) {
-                        continue;
-                    }
-                    if (square.isWhite()) {
-                        white = true;
-                    }
-                    if(square.isBlack()) {
-                        black = true;
-                    }
-                }
-                if(white && black)
-                    cellBoard[file][row] = CellState.SHARED;
-                else {
-                    if (white) {
-                        cellBoard[file][row] = CellState.SEEDED_WHITE;
-                    }
-                    else { //black
-                        cellBoard[file][row] = CellState.SEEDED_BLACK;
-                    }
-                }
-                break;
-            case OCCUPIED_WHITE:
-            case OCCUPIED_BLACK:
-                //TODO: needed?
-                break;
-            case SEEDED_WHITE:
-            case SEEDED_BLACK:
-                adjacent = getNeighbors(file, row);
-                for(CellState neighbor : adjacent) {
-                    if (neighbor.isDame())
-                        cellBoard[file][row] = CellState.CONTAMINATED;
-                    if(neighbor.isSeeded() && neighbor != state) {
-                        cellBoard[file][row] = CellState.CONTAMINATED;
-                    }
-                }
-                break;
-            case CONTAMINATED:
-                System.out.println("contaminated: " + file + ", " + row);
-                cellBoard[file][row] = CellState.SHARED;
-                break;
-        }
-    }
-
-    private void copyToRemembered() {
-        rememberedBoard = Arrays.copyOf(cellBoard, cellBoard.length);
-    }
-
-    protected List<CellState> getNeighbors(int file, int row) {
-//        return get4Neighbors(file, row);
-        return get8Neighbors(file, row);
-    }
-
-    private List<CellState> get8Neighbors(int file, int row) {
-        List<CellState> adjacent = get4Neighbors(file, row);
-
-        if(file > MIN_INDEX && row > MIN_INDEX)
-            adjacent.add(cellBoard[file-1][row-1]);
-        if(file > MIN_INDEX && row < MAX_INDEX)
-            adjacent.add(cellBoard[file-1][row+1]);
-        if(file < MAX_INDEX && row > MIN_INDEX)
-            adjacent.add(cellBoard[file+1][row-1]);
-        if(file < MAX_INDEX && row < MAX_INDEX)
-            adjacent.add(cellBoard[file+1][row+1]);
-
-        return adjacent;
-    }
-
-    private List<CellState> get4Neighbors(int file, int row) {
-        List<CellState> adjacent = new ArrayList<>();
-
-        if(file > MIN_INDEX)
-            adjacent.add(cellBoard[file-1][row]);
-        if(file < MAX_INDEX)
-            adjacent.add(cellBoard[file+1][row]);
-        if(row > MIN_INDEX)
-            adjacent.add(cellBoard[file][row-1]);
-        if(row < MAX_INDEX)
-            adjacent.add(cellBoard[file][row+1]);
-        return adjacent;
-    }
-
-    //FIXME: bad condition - should be no change
-    private boolean shouldContinueCalculation() {
-        Mutable<Boolean> should = new Mutable<>(false);
-        traverseBoard((i, j, state) -> {
-            if (state == CellState.EMPTY || state == CellState.CONTAMINATED) {
-                should.set(true);
-            }
-        });
-        return should.get();
-    }
-
     private void traverseBoard(Command method) {
         for(int i = 1; i < 10; i++) {
             for(int j = 1; j < 10; j++) {
@@ -174,26 +236,7 @@ public class TerritoryCalculatorImpl implements TerritoryCalculator {
         }
     }
 
-    private void copyStatesFromGameBoard() {
-        traverseBoard((i, j, state) -> {
-            Square square = board.get(i, j);
-            if (!square.isEmpty()) {
-                if(square.getPiece().isBlack())
-                    cellBoard[i][j] = CellState.OCCUPIED_BLACK;
-                else {
-                    assert square.getPiece().isWhite();
-                    cellBoard[i][j] = CellState.OCCUPIED_WHITE;
-                }
-            }
-        });
-    }
-
-    private void initializeCellBoard() {
-        traverseBoard((i, j, state) -> {
-            cellBoard[i][j] = CellState.EMPTY;
-        });
-    }
-
+    /*CONTAMINATED is not needed anymore*/
     /*
     * EMPTY - no piece on square
     * SEEDED - square analyzed (by growth algorithm)
@@ -202,10 +245,7 @@ public class TerritoryCalculatorImpl implements TerritoryCalculator {
     * CONTAMINATED - temporary state (from SEEDED to SHARED), used to tell when to terminate
     * */
     protected enum CellState {
-        EMPTY,
-        SEEDED_WHITE, SEEDED_BLACK,
-        OCCUPIED_WHITE, OCCUPIED_BLACK,
-        SHARED, CONTAMINATED;
+        EMPTY, SEEDED_WHITE, SEEDED_BLACK, OCCUPIED_WHITE, OCCUPIED_BLACK, SHARED, CONTAMINATED;
 
         public boolean isEmpty() {
             return this.equals(EMPTY);
@@ -226,10 +266,41 @@ public class TerritoryCalculatorImpl implements TerritoryCalculator {
         public boolean isDame() {
             return this.equals(SHARED) || this.equals(CONTAMINATED);
         }
+
+        //needed for debugging
+        @Override
+        public String toString() {
+            char rc = '?';
+            switch (this) {
+                case EMPTY:
+                    rc = 'E';
+                    break;
+                case SEEDED_WHITE:
+                    rc = 'W';
+                    break;
+                case SEEDED_BLACK:
+                    rc = 'B';
+                    break;
+                case OCCUPIED_WHITE:
+                    rc = 'ｏ';
+                    break;
+                case OCCUPIED_BLACK:
+                    rc = 'ꝋ';
+                    break;
+                case SHARED:
+                    rc = 'S';
+                    break;
+                case CONTAMINATED:
+                    rc = 'C';
+                    break;
+            }
+            return String.valueOf(rc);
+        }
+
     }
 
     protected interface Command {
-        public void execute(int x, int y, CellState state);
+        void execute(final int x, final int y, final CellState state);
     }
 
     public class Mutable<T> {
